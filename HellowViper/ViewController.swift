9ed38705ab5cf1f44ffcfc00def2d6ad7a6d3d0c
Eric Liu <eric.liu@uber.com>
2018-07-28 22:07:46 -0700

    Creates Assets folder

--- HellowViper/ViewController.swift
+++ HellowViper/ViewController.swift
@@ -12,16 +12,20 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+//        self.present(AppDefaultBuilder().main(), animated: false, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-
 
 
 }
